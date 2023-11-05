be6aeb616761aec3e6361cf5acce0500ecd35d08
Shubham-0022 <k74.shubham@gmail.com>
2023-11-05 10:33:53 +0530

    implemented all sorting algo

--- src/components/sorting/Sorting.jsx
+++ src/components/sorting/Sorting.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import "./Sorting.css"
 import { useParams } from 'react-router-dom'
 import { MergeSort } from './mergeSort/MergeSort';
+import { SelectionSort } from './SelectionSort/SelectionSort';
+import { BubbleSort } from './BubbleSort/BubbleSort';
+import { InsertionSort } from './InsertionSort/InsertionSort';
+import { QuickSort } from './QuickSort/QuickSort';
 
 export const Sorting = () => {
     const { page } = useParams();
   return (
     <div className='container'>
-        {page == 'mergesort' && <MergeSort />}
+        {page === 'mergesort' && <MergeSort />}
+        {page === "selectionsort" && <SelectionSort />}
+        {page === "bubblesort" && <BubbleSort />}
+        {page === "insertionsort" && <InsertionSort />}
+        {page === "quicksort" && <QuickSort />}
     </div>
   )
 }
